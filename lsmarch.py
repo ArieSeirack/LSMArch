330ce624e331eda67f9cef75d21374de9a7d8917
ArieSeirack <arieseirack@mail.ustc.edu.cn>
2020-02-06 13:14:26 +0800

    update for tree structure

--- lsmarch.py
+++ lsmarch.py
@@ -10,6 +10,7 @@ import sys
 from lav_encode import lavencode
 from lav_decode import lavdecode
 from lav_check import lavcheck
+from lav_stack import lavstack
 
 
 #  get parameters from command line
@@ -17,11 +18,13 @@ def initpara(argv):
     filename = ""
     comp = 0
     indir = ""
+    indirs = ""
     outdir = ""
     crf = ''
+    mode = ''
 
     try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["help", "encode", "decode", "check", "crf="])
+        opts, args = getopt.getopt(argv, "hi:o:m:", ["help", "encode", "decode", "check", "stack", "crf="])
     except getopt.GetoptError:
         print('Error: lsmarch.py -i <input_dir> -o <output_dir> -crf <crf_value>')
         sys.exit(2)
@@ -32,6 +35,7 @@ def initpara(argv):
             print("\nDecoding\npython lsmarch.py --decode -i <input_lav_file_name> -o <output_dir>")
             print("\nCheck the details of LAV file")
             print("\nDecoding\npython lsmarch.py --check -i <lav_file_name>")
+            print("\nStack\npython lsmarch.py --stack -i <input_lav_file_names> -m <mode> -o <output_file_name>")
             sys.exit()
 
         if opt == "--encode":
@@ -40,6 +44,8 @@ def initpara(argv):
             comp = 0
         elif opt == "--check":
             comp = 2
+        elif opt == "--stack":
+            comp = 3
 
         if opt == "-i":
             if comp == 1:
@@ -48,12 +54,19 @@ def initpara(argv):
                 filename = arg
             elif comp == 2:
                 filename = arg
+            elif comp == 3:
+                indirs = arg
 
         if opt == '-o':
             if comp == 1:
                 filename = arg
             elif comp == 0:
                 outdir = arg
+            elif comp == 3:
+                filename = arg
+
+        if opt == '-m':
+            mode = arg
 
         if opt == '--crf':
             crf = arg
@@ -67,6 +80,9 @@ def initpara(argv):
     elif comp == 2:
         print("input_file_name = %s.lav" % filename)
         return comp, filename
+    elif comp == 3:
+        print("input_lav_dirs = %s\noutput_file_name = %s.lav" % (indirs, filename))
+        return comp, indirs, mode, filename
 
 
 if __name__ == "__main__":
@@ -83,6 +99,11 @@ if __name__ == "__main__":
     elif para[0] == 2:
         filename = para[1]
         lavcheck(filename)
+    elif para[0] == 3:
+        indir = para[1]
+        mode = para[2]
+        outlavname = para[3]
+        lavstack(indir, mode, outlavname)
 
 
 
